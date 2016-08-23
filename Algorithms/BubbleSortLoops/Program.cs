4befdd987424fb37dd2d4883371b1f19a4edfaab
Sergey Okinchuk <serg3d@hotmail.com>
2016-08-23 15:36:33 -0700

    added BubbleSortLoop

---
+++ Algorithms/BubbleSortLoops/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSortLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[10];
+            
+            Random r = new Random();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = r.Next(0, 100);
+                Console.Write(arr[i] + " ");
+            }
+
+            //Console.ReadKey();
+
+            int[] newArr = sort(arr);
+            
+
+
+            Console.WriteLine();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(newArr[i] + " ");
+            }
+            Console.ReadKey();
+        }
+
+        public static int[] sort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 1; j > (n - j); j++)
+                {
+                    if (arr[j] < arr[j - 1])
+                    {
+                        int tmp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = tmp;
+                    }
+
+                }
+            }
+            return arr;
+        }
+    }
+}
