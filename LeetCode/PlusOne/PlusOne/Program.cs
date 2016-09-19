ea7413abc70e0215e41aac3f0aa3211b785daf94
Sergey Okinchuk <serg3d@hotmail.com>
2016-09-19 11:25:18 -0700

    initial commit C# for leetCode plusOne

---
+++ LeetCode/PlusOne/PlusOne/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlusOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {1, 3, 5, 9};
+
+            arr = plusOne(arr);
+
+            foreach (var i in arr)
+            {
+                  Console.Write(i + " ");
+            }
+        }
+
+        public static int[] plusOne(int[] arr)
+        {
+
+
+            return arr;
+        }
+
+
+    }
+}
